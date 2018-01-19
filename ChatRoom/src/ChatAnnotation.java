bf668f231fed3b2d99e8b2199a0b598ed4b93446
Yiheng Shu <lzw429@qq.com>
2018-01-19 10:37:02 +0800

    在线列表缺失问题

--- ChatRoom/src/ChatAnnotation.java
+++ ChatRoom/src/ChatAnnotation.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 
 @ServerEndpoint(value = "/ChatSocket")
 public class ChatAnnotation {
@@ -19,35 +20,43 @@ public class ChatAnnotation {
         // 把当前客户端对应的ChatAnnotation对象加入到集合中
         connections.add(this);
 
+        // 广播新用户加入群聊的信息
         String message = String.format("%s %s", session.getId(), "加入了群聊");
-        broadcast(message, session.getId(), LocalDateTime.now());
+        broadcast(message, session.getId(), LocalDateTime.now(), Message.LOGIN);
+
+        // 向新用户单播在线列表
+        String[] onlineList = getOnlineList();
+        Gson gson = new GsonBuilder().create();
+        String JsOnlineList = gson.toJson(onlineList);
+        unicast(JsOnlineList, "Server", session.getId(), LocalDateTime.now(), Message.LIST);
     }
 
     @OnMessage
     public void incoming(String content) throws IOException {
         // 消息推送给所有的客户端
         String message = String.format("%s %s %s", session.getId(), "：", content);
-        broadcast(message, session.getId(), LocalDateTime.now());
+        broadcast(message, session.getId(), LocalDateTime.now(), Message.NORMAL);
     }
 
     @OnClose
     public void end() {
+        // 连接中断时
         connections.remove(this);
         String message = String.format("%s %s", session.getId(), "连接已中断");
-        broadcast(message, session.getId(), LocalDateTime.now());
+        broadcast(message, session.getId(), LocalDateTime.now(), Message.QUIT);
     }
 
     @OnError
     public void onError(Throwable t) throws Throwable {
     }
 
-    private static void broadcast(String msg, String sender, LocalDateTime sendTime) {
-        Message Msg = new Message(msg, sender, sendTime);
+    private static void broadcast(String msg, String sender, LocalDateTime sendTime, int type) {
+        Message Msg = new Message(msg, sender, sendTime, type);
         Gson gson = new GsonBuilder().create();
         String msgToJson = gson.toJson(Msg);
         for (ChatAnnotation client : connections) {
             try {
-                synchronized (client) {
+                synchronized (client) { // 同步锁
                     client.session.getBasicRemote().sendText(msgToJson);
                 }
             } catch (IOException e) {
@@ -55,11 +64,38 @@ public class ChatAnnotation {
                 try {
                     client.session.close();
                 } catch (IOException e1) {
+                    // Ignore
                 }
                 String disconnect = String.format("%s %s", client.session.getId(), "连接已中断");
-                broadcast(disconnect, sender, LocalDateTime.now());
+                broadcast(disconnect, sender, LocalDateTime.now(), Message.QUIT);
             }
         }
     }
 
+    private static void unicast(String msg, String sender, String receiver, LocalDateTime sendTime, int type) {
+        Message Msg = new Message(msg, sender, receiver, sendTime, type);
+        Gson gson = new GsonBuilder().create();
+        String msgToJson = gson.toJson(Msg);
+        for (ChatAnnotation client : connections) {
+            if (client.session.getId().equals("receiver")) {
+                try {
+                    synchronized (client) { // 同步锁
+                        client.session.getBasicRemote().sendText(msgToJson);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static String[] getOnlineList() {
+        String[] onlineList = new String[connections.size() + 5];
+        int i = 0;
+        for (ChatAnnotation client : connections) {
+            onlineList[i++] = client.session.getId();
+        }
+        return onlineList;
+    }
 }
+
--- ChatRoom/src/Message.java
+++ ChatRoom/src/Message.java
@@ -5,6 +5,12 @@ public class Message {
     private String receiver = null;
     private String content;
     private LocalDateTime sendTime;
+    private int type;
+    public final static int LOGIN = 0; // 上线通知
+    public final static int NORMAL = 1; // 普通消息
+    public final static int QUIT = 2; // 离线通知
+    public final static int LIST = 3; // 在线列表
+
 
     public String getSender() {
         return sender;
@@ -38,19 +44,29 @@ public class Message {
         this.sendTime = sendTime;
     }
 
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
     // 构造方法
-    Message(String content, String sender, String receiver, LocalDateTime sendTime) {
+    Message(String content, String sender, String receiver, LocalDateTime sendTime, int type) {
         // 好友间消息
         this.content = content;
         this.sender = sender;
         this.receiver = receiver;
         this.sendTime = sendTime;
+        this.type = type;
     }
 
-    Message(String content, String sender, LocalDateTime sendTime) {
+    Message(String content, String sender, LocalDateTime sendTime, int type) {
         // 广播消息，不必记录接收者
         this.content = content;
         this.sender = sender;
         this.sendTime = sendTime;
+        this.type = type;
     }
 }
